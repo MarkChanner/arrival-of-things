b78f9f78e78f2abe110d0676a0d67ad537ed1254
Mark Channer <markchanner@gmail.com>
2019-07-13 13:45:49 +0100

    change baseURL to retrieve Great Portland Street arrivals

--- http-common.js
+++ http-common.js
@@ -5,5 +5,5 @@ const KINGS_CROSS_NAPTAN_ID = '940GZZLUKSX';
 const OXFORD_CIRCUS_NAPTAN_ID = '940GZZLUOXC'
 
 export const HTTP = axios.create({
-    baseURL: `https://api.tfl.gov.uk/StopPoint/${OXFORD_CIRCUS_NAPTAN_ID}/Arrivals?mode=tube`
+    baseURL: `https://api.tfl.gov.uk/StopPoint/${GT_PORTLAND_ST_NAPTAN_ID}/Arrivals?mode=tube`
 })
