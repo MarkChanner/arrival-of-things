d498226a180c339f77c17127cd8c0c82a78b6068
Mark Channer <markchanner@gmail.com>
2019-07-13 12:46:28 +0100

    add naptan id for Kings Cross

--- http-common.js
+++ http-common.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const GT_PORTLAND_ST_NAPTAN_ID = '940GZZLUGPS';
+const KINGS_CROSS_NAPTAN_ID = '940GZZLUKSX';
 
 export const HTTP = axios.create({
     baseURL: `https://api.tfl.gov.uk/StopPoint/${GT_PORTLAND_ST_NAPTAN_ID}/Arrivals?mode=tube`
