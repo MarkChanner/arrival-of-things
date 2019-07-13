738ae36099ff6059695700ab2d879ab63b74203e
Mark Channer <markchanner@gmail.com>
2019-07-13 12:52:52 +0100

    add methods for destination and time to station

--- src/components/Train.vue
+++ src/components/Train.vue
@@ -1,11 +1,20 @@
 <template>
-  <p>{{ train.destinationName }} - {{ train.timeToStation }}</p>
+  <p>{{ getDestination(train.destinationName) }} - {{ getTimeToStation(train.timeToStation) }}</p>
 </template>
 
 <script>
 export default {
   props: {
     train: Object
+  },
+  methods: {
+    getDestination(destination) {
+      return destination ? destination.split('Underground Station')[0] : 'Check front of train';
+    },
+    getTimeToStation(secondsToArrival) {
+      const mins = Math.floor(secondsToArrival / 60);
+      return mins > 0 ? `${mins} min` : 'due';
+    }
   }
 }
 </script>
